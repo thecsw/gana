a56fbaaa2e7fffe87045130462da6fd8bfb067f3
Matthew Sanetra <41018997+matthewsanetra@users.noreply.github.com>
2022-11-11 20:19:19 +0000

    Add initial tests

--- go.mod
+++ go.mod
@@ -3,3 +3,11 @@ module github.com/thecsw/gana
 go 1.18
 
 require golang.org/x/exp v0.0.0-20221110155412-d0897a79cd37
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
+	github.com/stretchr/testify v1.8.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ ops_test.go
@@ -0,0 +1,162 @@
+package echidna
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMin(t *testing.T) {
+	assert.Equal(t, Min(1, 2), 1)
+	assert.Equal(t, Min(2, 1), 1)
+	assert.Equal(t, Min(1, 1), 1)
+	assert.Equal(t, Min(1.0, 2.0), 1.0)
+	assert.Equal(t, Min(2.0, 1.0), 1.0)
+	assert.Equal(t, Min(1.0, 1.0), 1.0)
+	assert.Equal(t, Min("a", "b"), "a")
+	assert.Equal(t, Min("b", "a"), "a")
+	assert.Equal(t, Min("a", "a"), "a")
+}
+
+func TestMax(t *testing.T) {
+	assert.Equal(t, Max(1, 2), 2, "1, 2")
+	assert.Equal(t, Max(2, 1), 2, "2, 1")
+	assert.Equal(t, Max(1, 1), 1, "1, 1")
+	assert.Equal(t, Max(1.0, 2.0), 2.0, "1.0, 2.0")
+	assert.Equal(t, Max(2.0, 1.0), 2.0, "2.0, 1.0")
+	assert.Equal(t, Max(1.0, 1.0), 1.0, "1.0, 1.0")
+	assert.Equal(t, Max("a", "b"), "b", "a, b")
+	assert.Equal(t, Max("b", "a"), "b", "b, a")
+	assert.Equal(t, Max("a", "a"), "a", "a, a")
+}
+
+func TestFirst(t *testing.T) {
+	assert.Equal(t, First([]int{1, 2, 3}), 1, "1, 2, 3")
+	assert.Equal(t, First([]int{}), 0, "[]int{}")
+	assert.Equal(t, First([]float64{1.0, 2.0, 3.0}), 1.0, "1.0, 2.0, 3.0")
+	assert.Equal(t, First([]float64{}), 0.0, "[]float64{}")
+	assert.Equal(t, First([]string{"a", "b", "c"}), "a", `"a", "b", "c"`)
+	assert.Equal(t, First([]string{}), "", "[]string{}")
+}
+
+func TestLast(t *testing.T) {
+	assert.Equal(t, Last([]int{1, 2, 3}), 3, "1, 2, 3")
+	assert.Equal(t, Last([]int{}), 0, "[]int{}")
+	assert.Equal(t, Last([]float64{1.0, 2.0, 3.0}), 3.0, "1.0, 2.0, 3.0")
+	assert.Equal(t, Last([]float64{}), 0.0, "[]float64{}")
+	assert.Equal(t, Last([]string{"a", "b", "c"}), "c", `"a", "b", "c"`)
+	assert.Equal(t, Last([]string{}), "", "[]string{}")
+}
+
+func TestZeroValue(t *testing.T) {
+	assert.Equal(t, ZeroValue[int](), 0, "int")
+	assert.Equal(t, ZeroValue[float64](), 0.0, "float64")
+	assert.Equal(t, ZeroValue[string](), "", "string")
+}
+
+func TestMap(t *testing.T) {
+	assert.Equal(t, Map(func(x int) int { return x * 2 }, []int{1, 2, 3}), []int{2, 4, 6}, "1, 2, 3")
+	assert.Equal(t, Map(func(x float64) float64 { return x * 2 }, []float64{1.0, 2.0, 3.0}), []float64{2.0, 4.0, 6.0}, "1.0, 2.0, 3.0")
+	assert.Equal(t, Map(func(x string) string { return x + x }, []string{"a", "b", "c"}), []string{"aa", "bb", "cc"}, `"a", "b", "c"`)
+	assert.Equal(t, Map(func(x int) int { return x * 2 }, []int{}), []int{}, "[]int{}")
+	assert.Equal(t, Map(func(x float64) float64 { return x * 2 }, []float64{}), []float64{}, "[]float64{}")
+	assert.Equal(t, Map(func(x string) string { return x + x }, []string{}), []string{}, "[]string{}")
+}
+
+func TestFilter(t *testing.T) {
+	assert.Equal(t, Filter(func(x int) bool { return x > 2 }, []int{1, 2, 3}), []int{3}, "1, 2, 3")
+	assert.Equal(t, Filter(func(x float64) bool { return x > 2.0 }, []float64{1.0, 2.0, 3.0}), []float64{3.0}, "1.0, 2.0, 3.0")
+	assert.Equal(t, Filter(func(x string) bool { return x > "b" }, []string{"a", "b", "c"}), []string{"c"}, `"a", "b", "c"`)
+	assert.Equal(t, Filter(func(x int) bool { return x > 2 }, []int{}), []int{}, "[]int{}")
+	assert.Equal(t, Filter(func(x float64) bool { return x > 2.0 }, []float64{}), []float64{}, "[]float64{}")
+	assert.Equal(t, Filter(func(x string) bool { return x > "b" }, []string{}), []string{}, "[]string{}")
+}
+
+func TestTake(t *testing.T) {
+	assert.Equal(t, Take(2, []int{1, 2, 3}), []int{1, 2}, "1, 2, 3")
+	assert.Equal(t, Take(2, []float64{1.0, 2.0, 3.0}), []float64{1.0, 2.0}, "1.0, 2.0, 3.0")
+	assert.Equal(t, Take(2, []string{"a", "b", "c"}), []string{"a", "b"}, `"a", "b", "c"`)
+	assert.Equal(t, Take(2, []int{}), []int{}, "[]int{}")
+	assert.Equal(t, Take(2, []float64{}), []float64{}, "[]float64{}")
+	assert.Equal(t, Take(2, []string{}), []string{}, "[]string{}")
+}
+
+func TestTail(t *testing.T) {
+	assert.Equal(t, Tail(2, []int{1, 2, 3}), []int{2, 3}, "1, 2, 3")
+	assert.Equal(t, Tail(2, []float64{1.0, 2.0, 3.0}), []float64{2.0, 3.0}, "1.0, 2.0, 3.0")
+	assert.Equal(t, Tail(2, []string{"a", "b", "c"}), []string{"b", "c"}, `"a", "b", "c"`)
+	assert.Equal(t, Tail(2, []int{}), []int{}, "[]int{}")
+	assert.Equal(t, Tail(2, []float64{}), []float64{}, "[]float64{}")
+	assert.Equal(t, Tail(2, []string{}), []string{}, "[]string{}")
+}
+
+func TestDrop(t *testing.T) {
+	assert.Equal(t, Drop(2, []int{1, 2, 3}), []int{3}, "1, 2, 3")
+	assert.Equal(t, Drop(2, []float64{1.0, 2.0, 3.0}), []float64{3.0}, "1.0, 2.0, 3.0")
+	assert.Equal(t, Drop(2, []string{"a", "b", "c"}), []string{"c"}, `"a", "b", "c"`)
+	assert.Equal(t, Drop(2, []int{}), []int{}, "[]int{}")
+	assert.Equal(t, Drop(2, []float64{}), []float64{}, "[]float64{}")
+	assert.Equal(t, Drop(2, []string{}), []string{}, "[]string{}")
+}
+
+func TestDropString(t *testing.T) {
+	assert.Equal(t, DropString(1, "abc"), "bc", `"abc"[1:]`)
+	assert.Equal(t, DropString(2, "abc"), "c", `"abc"[2:]`)
+	assert.Equal(t, DropString(3, "abc"), "", `"abc"[3:]`)
+	assert.Equal(t, DropString(4, "abc"), "", `"abc"[4:]`)
+	assert.Equal(t, DropString(0, "abc"), "abc", `"abc"[0:]`)
+	assert.Equal(t, DropString(1, ""), "", `""[1:]`)
+}
+
+func TestAny(t *testing.T) {
+	assert.True(t, Any(2, []int{1, 2, 3}), "1, 2, 3")
+	assert.True(t, Any(2.0, []float64{1.0, 2.0, 3.0}), "1.0, 2.0, 3.0")
+	assert.True(t, Any("b", []string{"a", "b", "c"}), `"a", "b", "c"`)
+	assert.False(t, Any(4, []int{1, 2, 3}), "1, 2, 3")
+	assert.False(t, Any(4.0, []float64{1.0, 2.0, 3.0}), "1.0, 2.0, 3.0")
+	assert.False(t, Any("d", []string{"a", "b", "c"}), `"a", "b", "c"`)
+}
+
+func TestAnyf(t *testing.T) {
+	assert.True(t, Anyf(func(x int) bool { return x > 2 }, []int{1, 2, 3}), "1, 2, 3")
+	assert.True(t, Anyf(func(x float64) bool { return x > 2.0 }, []float64{1.0, 2.0, 3.0}), "1.0, 2.0, 3.0")
+	assert.True(t, Anyf(func(x string) bool { return x > "b" }, []string{"a", "b", "c"}), `"a", "b", "c"`)
+	assert.False(t, Anyf(func(x int) bool { return x > 4 }, []int{1, 2, 3}), "1, 2, 3")
+	assert.False(t, Anyf(func(x float64) bool { return x > 4.0 }, []float64{1.0, 2.0, 3.0}), "1.0, 2.0, 3.0")
+	assert.False(t, Anyf(func(x string) bool { return x > "d" }, []string{"a", "b", "c"}), `"a", "b", "c"`)
+}
+
+func TestAll(t *testing.T) {
+	assert.False(t, All(2, []int{1, 2, 3}), "1, 2, 3")
+	assert.False(t, All(2.0, []float64{1.0, 2.0, 3.0}), "1.0, 2.0, 3.0")
+	assert.False(t, All("b", []string{"a", "b", "c"}), `"a", "b", "c"`)
+	assert.True(t, All(20, Repeat(20, 10)), "Repeat(20, 10)")
+	assert.True(t, All(20.0, Repeat(20.0, 10)), "Repeat(20.0, 10)")
+	assert.True(t, All("b", Repeat("b", 10)), `Repeat("b", 10)`)
+}
+
+func TestAllf(t *testing.T) {
+	assert.False(t, Allf(func(x int) bool { return x > 2 }, []int{1, 2, 3}), "1, 2, 3")
+	assert.False(t, Allf(func(x float64) bool { return x > 2.0 }, []float64{1.0, 2.0, 3.0}), "1.0, 2.0, 3.0")
+	assert.False(t, Allf(func(x string) bool { return x > "b" }, []string{"a", "b", "c"}), `"a", "b", "c"`)
+	assert.True(t, Allf(func(x int) bool { return x > 0 }, Repeat(20, 10)), "Repeat(20, 10)")
+	assert.True(t, Allf(func(x float64) bool { return x > 0.0 }, Repeat(20.0, 10)), "Repeat(20.0, 10)")
+	assert.True(t, Allf(func(x string) bool { return x > "a" }, Repeat("b", 10)), `Repeat("b", 10)`)
+}
+
+func TestRepeat(t *testing.T) {
+	assert.Equal(t, Repeat(1, 0), []int{}, "Repeat(1, 0)")
+	assert.Equal(t, Repeat(1, 1), []int{1}, "Repeat(1, 1)")
+	assert.Equal(t, Repeat(1, 2), []int{1, 1}, "Repeat(1, 2)")
+	assert.Equal(t, Repeat(1, 3), []int{1, 1, 1}, "Repeat(1, 3)")
+	assert.Equal(t, Repeat(1, 4), []int{1, 1, 1, 1}, "Repeat(1, 4)")
+	assert.Equal(t, Repeat(1, 5), []int{1, 1, 1, 1, 1}, "Repeat(1, 5)")
+}
+
+func TestGetPointer(t *testing.T) {
+	var i int
+	i_ptr := &i
+	i_ptr2 := &i_ptr
+	assert.Equal(t, GetPointer(i), i_ptr, "GetPointer(i)")
+	assert.Equal(t, GetPointer(i_ptr), i_ptr2, "GetPointer(&i)")
+}
