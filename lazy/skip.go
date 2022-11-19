abc03908d85765b90568e1a2c7fea6b118a0ff55
Matthew Sanetra <41018997+matthewsanetra@users.noreply.github.com>
2022-11-19 12:44:07 +0000

    Add SkipWhile

--- lazy/skip.go
+++ lazy/skip.go
@@ -7,8 +7,15 @@ type SkipT[T any] struct {
 	n    int
 }
 
-// Make sure that SkipT implements the Iter interface.
+type SkipWhileT[T any] struct {
+	iter    Iter[T]
+	pred    func(T) bool
+	skipped bool
+}
+
+// Make sure that SkipT and SkipWhileT implements the Iter interface.
 var _ Iter[string] = (*SkipT[string])(nil)
+var _ Iter[string] = (*SkipWhileT[string])(nil)
 
 // Skip returns an iterator that skips the first n elements of the given iterator.
 func Skip[T any](iter Iter[T], n int) Iter[T] {
@@ -22,6 +29,25 @@ func (s *SkipT[T]) Next() (T, bool) {
 			return gana.ZeroValue[T](), false
 		}
 		s.n--
+	}
+	return s.iter.Next()
+}
+
+// SkipWhile returns an iterator that skips the first group of elements that satisfy the predicate.
+func SkipWhile[T any](iter Iter[T], pred func(T) bool) Iter[T] {
+	return &SkipWhileT[T]{iter, pred, false}
+}
+
+func (s *SkipWhileT[T]) Next() (T, bool) {
+	for !s.skipped {
+		e, ok := s.iter.Next()
+		if !ok {
+			return gana.ZeroValue[T](), false
+		}
+		if !s.pred(e) {
+			s.skipped = true
+			return e, true
+		}
 	}
 	return s.iter.Next()
 }
--- lazy/skip_test.go
+++ lazy/skip_test.go
@@ -28,3 +28,16 @@ func TestSkip(t *testing.T) {
 	result = Collect(Skip(iter, 2))
 	assert.Equal(t, []string{}, result)
 }
+
+func TestSkipWhile(t *testing.T) {
+	iter := NewIter([]string{"a", "b", "c"})
+	result := Collect(SkipWhile(iter, func(s string) bool { return s != "b" }))
+	assert.Equal(t, []string{"b", "c"}, result)
+
+	iter = NewIter([]string{"a", "b", "c"})
+	s, ok := iter.Next()
+	assert.Equal(t, "a", s)
+	assert.True(t, ok)
+	result = Collect(SkipWhile(iter, func(s string) bool { return s == "b" }))
+	assert.Equal(t, []string{"c"}, result)
+}
