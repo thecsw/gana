58f1b97fe49a9b22c4b3505c9b7078debfe41935
Matthew Sanetra <41018997+matthewsanetra@users.noreply.github.com>
2022-11-19 20:50:39 +0000

    Add tests

---
+++ tuple_test.go
@@ -0,0 +1,19 @@
+package gana
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUnpack(t *testing.T) {
+	a, b := (&Tuple[int, int]{1, 2}).Unpack()
+	assert.Equal(t, 1, a)
+	assert.Equal(t, 2, b)
+}
+
+func TestUnpackRef(t *testing.T) {
+	a, b := (&Tuple[int, int]{1, 2}).UnpackRef()
+	assert.Equal(t, 1, *a)
+	assert.Equal(t, 2, *b)
+}
