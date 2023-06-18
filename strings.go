de77ae89840465864bd731dce11152bb7a6005d0
Sandy <sandy@sandyuraz.com>
2023-06-18 14:56:31 -0500

    Count strings and runes from left to right
    Signed-off-by: Sandy <sandy@sandyuraz.com>

--- strings.go
+++ strings.go
@@ -35,3 +35,51 @@ func SkipString[T constraints.Unsigned](num T, what string) string {
 	}
 	return what[num:]
 }
+
+// CountRunesLeft counts the number of times a rune appears
+// at the beginning of a string.
+func CountRunesLeft[T constraints.Unsigned](s string, r rune) T {
+	count := T(0)
+	for _, rr := range s {
+		if rr != r {
+			return count
+		}
+		count++
+	}
+	return count
+}
+
+// CountRunesRight counts the number of times a rune appears
+// at the end of a string.
+func CountRunesRight[T constraints.Unsigned](s string, r rune) T {
+	count := T(0)
+	for i := len(s) - 1; i >= 0; i-- {
+		if rune(s[i]) != r {
+			return count
+		}
+		count++
+	}
+	return count
+}
+
+// CountStringsLeft counts the number of times a substring appears
+// at the beginning of a string.
+func CountStringsLeft[T constraints.Unsigned](s, substr string) T {
+	count := T(0)
+	for strings.HasPrefix(s, substr) {
+		count++
+		s = strings.TrimPrefix(s, substr)
+	}
+	return count
+}
+
+// CountStringsRight counts the number of times a substring appears
+// at the end of a string.
+func CountStringsRight[T constraints.Unsigned](s, substr string) T {
+	count := T(0)
+	for strings.HasSuffix(s, substr) {
+		count++
+		s = strings.TrimSuffix(s, substr)
+	}
+	return count
+}
--- strings_test.go
+++ strings_test.go
@@ -41,3 +41,35 @@ func TestSkipString(t *testing.T) {
 	assert.Equal(t, "abc", SkipString[uint](0, "abc"), `"abc"[0:]`)
 	assert.Equal(t, "", SkipString[uint](1, ""), `""[1:]`)
 }
+
+func TestCountRunesLeft(t *testing.T) {
+	assert.Equal(t, uint(2), CountRunesLeft[uint]("aabc", 'a'), `CountRunesLeft("aabc", 'a')`)
+	assert.Equal(t, uint(1), CountRunesLeft[uint]("bc", 'b'), `CountRunesLeft("bc", 'b')`)
+	assert.Equal(t, uint(0), CountRunesLeft[uint]("aabc", 'c'), `CountRunesLeft("aabc", 'c')`)
+	assert.Equal(t, uint(0), CountRunesLeft[uint]("aabc", 'd'), `CountRunesLeft("aabc", 'd')`)
+	assert.Equal(t, uint(0), CountRunesLeft[uint]("", 'a'), `CountRunesLeft("", 'a')`)
+}
+
+func TestCountRunesRight(t *testing.T) {
+	assert.Equal(t, uint(2), CountRunesRight[uint]("aabcc", 'c'), `CountRunesRight("aabcc", 'c')`)
+	assert.Equal(t, uint(1), CountRunesRight[uint]("aab", 'b'), `CountRunesRight("aab", 'b')`)
+	assert.Equal(t, uint(0), CountRunesRight[uint]("aabc", 'a'), `CountRunesRight("aabc", 'a')`)
+	assert.Equal(t, uint(0), CountRunesRight[uint]("aabc", 'd'), `CountRunesRight("aabc", 'd')`)
+	assert.Equal(t, uint(0), CountRunesRight[uint]("", 'a'), `CountRunesRight("", 'a')`)
+}
+
+func TestCountStringsLeft(t *testing.T) {
+	assert.Equal(t, uint(1), CountStringsLeft[uint]("aabc", "aa"), `CountStringsLeft("aabc", "aa")`)
+	assert.Equal(t, uint(3), CountStringsLeft[uint]("bcbcbca", "bc"), `CountStringsLeft("bcbcbca", "b")`)
+	assert.Equal(t, uint(0), CountStringsLeft[uint]("aabc", "c"), `CountStringsLeft("aabc", "c")`)
+	assert.Equal(t, uint(0), CountStringsLeft[uint]("aabc", "d"), `CountStringsLeft("aabc", "d")`)
+	assert.Equal(t, uint(0), CountStringsLeft[uint]("", "a"), `CountStringsLeft("", "a")`)
+}
+
+func TestCountStringsRight(t *testing.T) {
+	assert.Equal(t, uint(1), CountStringsRight[uint]("aabc", "bc"), `CountStringsRight("aabc", "bc")`)
+	assert.Equal(t, uint(3), CountStringsRight[uint]("abcbcbc", "bc"), `CountStringsRight("abcbcbc", "bc")`)
+	assert.Equal(t, uint(0), CountStringsRight[uint]("aabc", "a"), `CountStringsRight("aabc", "a")`)
+	assert.Equal(t, uint(0), CountStringsRight[uint]("aabc", "d"), `CountStringsRight("aabc", "d")`)
+	assert.Equal(t, uint(0), CountStringsRight[uint]("", "a"), `CountStringsRight("", "a")`)
+}
